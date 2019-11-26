7f4e256ab581a99bcd46ab43125b4713809c8e7a
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-26 17:19:25 +0100

    Add barebone index.js, listening on specified port

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
